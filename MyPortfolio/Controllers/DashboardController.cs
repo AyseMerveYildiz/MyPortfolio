5ae3f119f4dc850213baa37bd936b669d27bcd10
aysemerveyildiz <meerve.ylz@gmail.com>
2025-04-13 20:36:34 +0300

    Add project files.

---
+++ MyPortfolio/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class DashboardController : Controller
+    {
+        MyPortfolioContext context=new MyPortfolioContext();
+        public IActionResult Index()
+        {
+			ViewBag.toDoListCount = context.ToDoLists.Count();
+			ViewBag.doneToDoList = context.ToDoLists.Where(x => x.Status == true).Count();
+			ViewBag.unDoneToDoList = context.ToDoLists.Where(x => x.Status == false).Count();
+			ViewBag.skillCount = context.Skills.Count();
+			ViewBag.messageCount = context.Messages.Count();
+			ViewBag.readMessage = context.Messages.Where(x => x.IsRead == true).Count();
+			ViewBag.unReadMessage = context.Messages.Where(x => x.IsRead == false).Count();
+
+			ViewBag.portfolioCount = context.Portfolios.Count();
+			ViewBag.testimonialCount = context.Testimonials.Count();
+
+			var values = context.Skills.ToList();
+			return View(values);
+		}
+    }
+}
